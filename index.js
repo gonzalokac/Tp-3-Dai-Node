ae3080b56035029efa5b609ee0c79fe8e5ae1c94
gonzalokac <48045290@est.ort.edu.ar>
2025-03-31 09:34:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+
+ //console.log("Hola");
+
+/*function colores() {
+    let Array = ["rojo", "azul"];
+
+    for (let i = 0; i < Array.length; i++) {
+        console.log(Array[i]);
+    }
+}
+
+colores();  
+
+
+
+let verde=3;
+
+let amarillo = verde => verde + 1;
+
+console.log(amarillo(verde));
+
+let Array2 = ["perro", "gato"];
+
+let mascotaN = (arr) => {
+    arr.push("pez");
+};
+mascotaN(Array2); 
+console.log(Array2[2]); 
+
+
+
+let Array3 = ["Casa", "Techo", "Ventana", "Casa"];
+
+let posicion = Array3.lastIndexOf("Casa"); 
+
+console.log(posicion); 
+
+
+
+let Array4=[25,30]
+let Show =0;
+for(let i=0; i<2; i++){
+   Show+=Array4[i];
+    
+}
+console.log(Show);
+
+
+let Array5 = ["pri", "segu"];
+
+Array5.reverse(); 
+
+console.log(Array5); 
+
+ 
+let Array6 = [37, 21, 56, 2];
+
+Array6.sort((a, b) => a - b);
+
+console.log(Array6); 
+
+
+let Array7=[45,76,89,34,57,89]
+
+for(let i=0; i<Array7.length; i++){
+
+   if (Array7[i]>50){
+
+    console.log(Array7[i]);
+
+   }
+
+   
+     
+ }
+
+  
+let contador=0;
+ let Array8=[3,4,3,7,8,3,5,3,9,3];
+
+ for(let i=0; i<Array8.length; i++){
+
+    if (Array8[i]===3){
+ 
+     contador+=1;
+ 
+    } }
+    console.log(contador);
+    */
