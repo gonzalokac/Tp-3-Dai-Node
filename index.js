0c27997a77a03bc0379e387a08debf4825da7f69
gonzalokac <48045290@est.ort.edu.ar>
2025-05-05 08:28:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
+ 
+/*
+console.log("hola");
 
- //console.log("Hola");
-
-/*function colores() {
+function colores() {
     let Array = ["rojo", "azul"];
-
-    for (let i = 0; i < Array.length; i++) {
-        console.log(Array[i]);
-    }
+    Array.forEach(color => console.log(color));
 }
+colores();
 
-colores();  
-
-
-
-let verde=3;
+let verde = 3;
 
 let amarillo = verde => verde + 1;
-
 console.log(amarillo(verde));
 
 let Array2 = ["perro", "gato"];
@@ -24,66 +18,29 @@ let Array2 = ["perro", "gato"];
 let mascotaN = (arr) => {
     arr.push("pez");
 };
-mascotaN(Array2); 
-console.log(Array2[2]); 
-
-
+mascotaN(Array2);
+console.log(Array2[2]);
 
 let Array3 = ["Casa", "Techo", "Ventana", "Casa"];
+let posicion = Array3.lastIndexOf("Casa");
+console.log(posicion);
 
-let posicion = Array3.lastIndexOf("Casa"); 
-
-console.log(posicion); 
-
-
-
-let Array4=[25,30]
-let Show =0;
-for(let i=0; i<2; i++){
-   Show+=Array4[i];
-    
-}
+let Array4 = [25, 30];
+let Show = Array4.reduce((acc, num) => acc + num, 0);
 console.log(Show);
 
-
 let Array5 = ["pri", "segu"];
+Array5.reverse();
+console.log(Array5);
 
-Array5.reverse(); 
-
-console.log(Array5); 
-
- 
 let Array6 = [37, 21, 56, 2];
-
 Array6.sort((a, b) => a - b);
+console.log(Array6);
 
-console.log(Array6); 
+let Array7 = [45, 76, 89, 34, 57, 89];
+Array7.filter(num => num > 50).forEach(num => console.log(num));
 
-
-let Array7=[45,76,89,34,57,89]
-
-for(let i=0; i<Array7.length; i++){
-
-   if (Array7[i]>50){
-
-    console.log(Array7[i]);
-
-   }
-
-   
-     
- }
-
-  
-let contador=0;
- let Array8=[3,4,3,7,8,3,5,3,9,3];
-
- for(let i=0; i<Array8.length; i++){
-
-    if (Array8[i]===3){
- 
-     contador+=1;
- 
-    } }
-    console.log(contador);
-    */
+let Array8 = [3, 4, 3, 7, 8, 3, 5, 3, 9, 3];
+let contador = Array8.filter(num => num === 3).length;
+console.log(contador);
+*/
